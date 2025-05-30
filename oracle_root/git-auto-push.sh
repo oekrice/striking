5435c4cc6e6902a00a5aee50958459b4ecb5ae9a
Oliver Rice <oliver@brenda.com>
2025-05-30 22:37:13 +0000

    Stopped tracking frequency data

--- oracle_root/git-auto-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/opc/striking || exit 1
-
-# Stage, commit, and push
-git add .
-git commit -m "Daily auto-commit: $(date '+%Y-%m-%d %H:%M:%S')" || exit 0
-git push origin data_backups
